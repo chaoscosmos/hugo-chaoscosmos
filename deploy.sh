cb47c6a433fbcf0ed7739c57e1239fc0a28c05b3
Chao Shi <chaoshi@gmail.com>
2018-07-07 20:05:47 +0800

    rebuilding site Sat Jul  7 20:05:47 CST 2018

--- deploy.sh
+++ deploy.sh
@@ -2,37 +2,39 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Go To Public folder
-#cd public
-# Add changes to git.
-#git add .
+SOURCE_DIR="./"
+PUBLISH_DIR="../chaoscosmos.github.io/"
 
 function git_push() {
-    # Commit changes.
+  # TODO need to validate before add and commit
+  # add all files in stage
   git add --all
+  # Commit changes.
   msg="rebuilding site `date`"
   if [ $# -eq 1 ]
     then msg="$1"
   fi
   git commit -m "$msg"
 
-  # Push source and build repos.
+  # Push to repo.
   git push origin master
 }
 
-# Come Back up to the Project Root
-#cd ..
-
-## 1st step: commit and push to source dir
-
-## 2nd step: commit and push to static dir
-# Build the project.
-# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+#---------MAIN-----------
+#-- 1st step: commit and push to source dir
+#-- 2nd step: commit and push to static dir
 # Build the project and copy to destination dir
-hugo -d ../chaoscosmos.github.io/
+hugo -d ${PUBLISH_DIR}
 
-if [ $# -eq 1 ]; then
-  git_push $1 
-else
-  git_push
-fi
+back_dir=`pwd`
+for repo_dir in ${SOURCE_DIR} ${PUBLISH_DIR}
+do
+  echo -e "\033[0;32mDeploying updates in ${repo_dir} to GitHub...\033[0m"
+  cd ${repo_dir}
+  if [ $# -eq 1 ]; then
+    git_push $1 
+  else
+    git_push
+  fi
+  cd ${back_dir}
+done
